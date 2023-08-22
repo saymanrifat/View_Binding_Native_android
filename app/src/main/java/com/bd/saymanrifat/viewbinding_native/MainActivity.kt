0cb17af9740d27633dfe281f07f7d244c7818a1a
Sayman Rifat <98202630+saymanrifat@users.noreply.github.com>
2023-08-22 20:42:58 +0600

    Initial commit

---
+++ app/src/main/java/com/bd/saymanrifat/viewbinding_native/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bd.saymanrifat.viewbinding_native
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
